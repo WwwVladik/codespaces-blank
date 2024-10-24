b9bb5455d0c95f43b16437a696b6b644074e91f7
WwwVladik <vlad.shalaev1@gmail.com>
2024-10-24 14:53:20 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+window.onload = function()
+{
+    const name = "Шалаев Владислав Станиславовичвич";
+    const element = document.querySelector("#name");
+    if (element)
+    {
+        element.textContent = name;
+    }
+}
